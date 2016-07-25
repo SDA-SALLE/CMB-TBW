cbf460309baef4598119f932ea0a8f77906cec28
Cesar Augusto Palacios Alarcon <ceapalaciosal@gmail.com>
2016-07-25 11:37:51 -0500

    Se elimina bindign speciacion
    - Se suprime la función binding speciation, debido a que no va a ser
    tenia en cuenta y que dicho objetivo lo reemplaza UNIONS.
    
    - Se crea y se prueba la función Total Emisión, el cual tienen el
    objetivo de sacar los totales de las emisiones.

--- CMB - TBW/src/main.py
+++ CMB - TBW/src/main.py
@@ -18,7 +18,6 @@ folderout = os.path.join ('..', 'data','out' ,'')
 clear(folderout)
 
 print 'Start Process'
-#convert()
 archiveflows = os.path.join('..', 'data', 'in','Flows', 'promFinal.csv');
 
 archivelinkprincipal = os.path.join('..', 'data', 'in', 'link', 'PRINCIPAL', 'PRINCIPALES_1.xlsx');
@@ -26,9 +25,9 @@ archivelinksecondary = os.path.join('..', 'data', 'in', 'link', 'SECUNDARIAS','S
 #archivelinktm = os.path.join('..', 'data', 'in', 'Link', 'TM', 'TM.xlsx');
 
 binding(archiveflows, archivelinkprincipal, 'principal')
-##print 'binding Principal Listo'
+print 'binding Principal, Ok'
 #binding(archiveflows, archivelinktm, 'TM')
-##print 'binding Transmilenio Listo'
+#print 'binding Transmilenio Listo'
 Type = 'HABIL'
 bindingsecondary(archiveflows, archivelinksecondary, Type)
 Type = 'NOHAB'
@@ -37,7 +36,7 @@ bindingsecondary(archiveflows, archivelinksecondary, Type)
 archive1 = os.path.join('..', 'data', 'in', 'link', 'SECUNDARIAS', 'secundary_HABIL_binding.csv')
 archive2 = os.path.join('..', 'data', 'in', 'link', 'SECUNDARIAS', 'secundary_NOHAB_binding.csv')
 unions(archive1, archive2)
-print 'binding Secundarias Listo'
+print 'binding Secundarias, Ok'
 
 
 FactorEmissions = os.path.join('..', 'data', 'in','EmissionFactors', 'FactoresEmision.xlsx')
@@ -49,7 +48,7 @@ calculation(principal, 'Principal', FactorEmissions, 'Habil')
 calculation(principal, 'Principal', FactorEmissions, 'NHabil')
 calculation(principal, 'Principal', FactoresEmissionBrake, 'Habil')
 calculation(principal, 'Principal', FactoresEmissionBrake, 'NHabil')
-print 'Calculo para Principales Habil y No Habil Listos'
+print 'Calculo para Principales Habil y No Habil, Ok'
 
 # TM = os.path.join('..', 'data', 'in','Link', 'TM', 'TMbinding.csv')
 # calculation(TM, 'TM', FactorEmissions, 'Habil')
@@ -63,7 +62,7 @@ calculation(secundarias, 'Secundary', FactorEmissions, 'Habil')
 calculation(secundarias, 'Secundary', FactorEmissions, 'NHabil')
 calculation(secundarias, 'Secundary', FactoresEmissionBrake, 'Habil')
 calculation(secundarias, 'Secundary', FactoresEmissionBrake, 'NHabil')
-print 'Calculo para Secundarias Habil y No Habil Listos'
+print 'Calculo para Secundarias Habil y No Habil, Ok'
 
 # folderdeparture = os.path.join('..', 'data','out', 'departure', '', '')
 # listFilesDeparture = list(folderdeparture)	
@@ -124,55 +123,23 @@ final(ArchiveNHabilWear)
 final(ArchiveNHabilConbustion)
 
 print 'speciation PM2.5 BRAKE'
-archivespeciation = os.path.join('..', 'data', 'in', 'Speciation', 'BRAKE_SCP_PROF_PM25.xlsx')
+archivespeciation = os.path.join('..', 'data', 'in', 'speciation', 'BRAKE_SCP_PROF_PM25.xlsx')
 folderwear = os.path.join('..', 'data','out', 'emissions', 'grid', 'wear', '')
 speciationwear(archivespeciation, folderwear)
-print 'Testing'
+print 'speciation, OK'
+print 'Init Testing'
 testing('PM2.5 BRAKE')
 
 print 'speciation PM2.5 TIRE'
-archivespeciation = os.path.join('..', 'data', 'in', 'Speciation', 'TIRE_SCP_PROF_PM25.xlsx')
+archivespeciation = os.path.join('..', 'data', 'in', 'speciation', 'TIRE_SCP_PROF_PM25.xlsx')
 folderwear = os.path.join('..', 'data','out', 'emissions', 'grid', 'wear', '')
 speciationwear(archivespeciation, folderwear)
-print 'Testing'
+print 'speciation, OK'
+print 'Init Testing'
 testing('PM2.5 TIRE')
 
 foldercombustion = os.path.join('..', 'data','out', 'emissions', 'grid', 'combustion', '')
 speciationcombustion(foldercombustion)
-
-#brinding speciation in folder /out/speciation/brinding
-#print 'Start brinding speciation'
-#foldercombustion = os.path.join('..', 'data','out', 'speciation', 'combustion', '')
-#bindingspeciation(foldercombustion, 'combustion')	
-
-#folderwear = os.path.join('..', 'data','out', 'speciation', 'wear', '')
-#bindingspeciation(folderwear, 'wear')	
-
-#foldercombustion = os.path.join('..', 'data','out', 'speciation', 'binding', 'combustion', '')
-#archivescombustion = list(foldercombustion)
-
-#for combustion in archivescombustion:
-	#archive = foldercombustion + combustion
-	#final(archive)
-
-#folderwearTIRE = os.path.join('..', 'data','out', 'speciation', 'binding', 'wear', 'TIRE', '')
-#archiveswearTIRE = list(folderwearTIRE)
-
-#for wear in archiveswearTIRE:
-	#archive = folderwearTIRE + wear
-	#final(archive)
-
-#folderwearBRAKE = os.path.join('..', 'data','out', 'speciation', 'binding', 'wear', 'BRAKE', '')
-#archiveswearBRAKE = list(folderwearBRAKE)
-
-#for wear in archiveswearBRAKE:
-	#archive = folderwearBRAKE + wear
-	#final(archive)
-
-#print 'binding speciation OK'
-#print '*------------------------------------*'
-#print 'Archivo NoHabil Listo'
-print 'End'
 
 categoryVechiclegrid()
 categoryCarburantgrid()
@@ -197,4 +164,4 @@ listapmc = list(folderout)
 for archive in listapmc:
 	archive = folderout + archive
 	final(archive)
-print 'END PMC'
+print 'PMC, OK'
--- CMB - TBW/src/totalEmissions.py
+++ CMB - TBW/src/totalEmissions.py
@@ -33,7 +33,9 @@ def writefull(full, noun, Type):
 	elif Type == 'wear': 
 		folder = os.path.join('..', 'data', 'out', 'TotalEmissions', 'wear', '')
 
+
 	csvsalida = open(folder + noun + "_E(TYear).csv", 'w')
+	#print csvsalida
 	salida = csv.writer(csvsalida, delimiter=',')
 
 	salida.writerow(['[ton/yr]'])
@@ -44,43 +46,70 @@ def writefull(full, noun, Type):
 
 def full(archive, noun, Type):
 	archiveDays = os.path.join('..', 'data', 'in', 'constants', 'DAYS.xlsx')
+	
 	Mdays = convertXLSCSV(archiveDays)
 	DH = int(float(Mdays[0][1]))
 	DNH = int(float(Mdays[1][1]))
 
-	matriz = convertCSVMatriz(archive)
-	head = matriz[0,:]
+	archivePrincipal = archive
+	matrizHabilPrincipal = convertCSVMatriz(archivePrincipal)
+	archivePrincipal = archive.replace('Habil', 'NHabil')
+	matrizNHabilPrincipal = convertCSVMatriz(archivePrincipal)
+
+	archiveSecundary = archive.replace('Principal', 'Secundary')
+	#print archiveSecundary
+	matrizHabilSecundary = convertCSVMatriz(archiveSecundary)
+	archiveSecundary = archive.replace('Habil', 'NHabil')
+	matrizNHabilSecundary = convertCSVMatriz(archiveSecundary)
+
+	head = matrizHabilPrincipal[0,:]
+	
 	index = 0
 	full = 0
+	fullHabilPrincipal = 0
+	fullNHabilPrincipal = 0
+	fullHabilSecundary = 0
+	fullNHabilSecundary = 0
 
 	for value in head:
 		if value == 'UNIT': 
 			colInit = index + 1
 		if value == 'E24h':
 			colEnd = index
-
 		index += 1 
 
-	for i in range(1, matriz.shape[0]):
+	for i in range(1, matrizHabilPrincipal.shape[0]):
 		for x in range(colInit, colEnd):
-			full += float(matriz[i][x])
+			fullHabilPrincipal += float(matrizHabilPrincipal[i][x])
+
+	for i in range(1, matrizNHabilPrincipal.shape[0]):
+		for x in range(colInit, colEnd):
+			fullNHabilPrincipal += float(matrizNHabilPrincipal[i][x])
+
+
+	for i in range(1, matrizHabilSecundary.shape[0]):
+		for x in range(colInit, colEnd):
+			fullHabilSecundary += float(matrizHabilSecundary[i][x])
+
+	for i in range(1, matrizNHabilSecundary.shape[0]):
+		for x in range(colInit, colEnd):
+			fullNHabilSecundary += float(matrizNHabilSecundary[i][x])
 
 	noun = noun.strip('.csv')
+	noun = noun.strip('_Habil')
+	noun = noun.replace('Principal', 'Emission')
+
 	index = 0
-	for i in noun: 
-		if i == '_':
-			init = index
-		index += 1
+	
+	fullHabilPrincipal = fullHabilPrincipal * DH / 1000000
+	fullNHabilPrincipal = fullNHabilPrincipal * DNH / 1000000
 
-	identy = noun[init+1:]
-	#print identy
-	if identy == 'NHabil': 
-		#print noun
-		full = full * DNH / 1000000
-	if identy == 'Habil': 
-		full = full * DH / 1000000
+	fullHabilSecundary = fullHabilSecundary * DH / 1000000
+	fullNHabilSecundary = fullNHabilSecundary * DNH / 1000000
 
-
+	
+	full = fullHabilPrincipal + fullNHabilPrincipal + fullHabilSecundary + fullNHabilSecundary 
+	#print fullHabil, '+' ,fullNHabil, '=', full
 	writefull(str(full), noun, Type)
 
 def totalemission(folder):
@@ -93,5 +122,7 @@ def totalemission(folder):
 		Type = 'wear'
 
 	for name in listEmissions:
-		archive = os.path.join(folder, name)
-		full(archive, name, Type)
+		if '_Habil' in name and 'Principal' in name: 
+			#print 'Paso'
+			archive = os.path.join(folder, name)
+			full(archive, name, Type)
